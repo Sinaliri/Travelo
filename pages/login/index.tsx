e5bcc0262cce13ee6a22a35db78be1802007ea5e
Sina <sina.liri@yahoo.com>
2023-06-06 03:18:20 +0330

    Layout Completed loginComponent Remain

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+//picture
+import LoginSvg from "../../assets/images/Login.svg"
+//components
+import EnterLayout from "@/Components/layout/EnteredLayout/EnterLayout"
+import SignUp from "../../Components/modules/Signup/Signup"
 const login = () => {
   return (
-    <div>login</div>
+    <EnterLayout imgsrc={LoginSvg as string}>
+        {/* complete login component and replace it with signup component */}
+        <SignUp/>
+    </EnterLayout>
   )
 }
 
