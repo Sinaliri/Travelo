90e04056746e4ef710b3681b558546a19916d0a1
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-06-06 23:23:14 +0330

    developed login page completely

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,11 +3,12 @@ import LoginSvg from "../../assets/images/Login.svg"
 //components
 import EnterLayout from "@/Components/layout/EnteredLayout/EnterLayout"
 import SignUp from "../../Components/modules/Signup/Signup"
+import Login from "@/Components/modules/Login/Login"
 const login = () => {
   return (
     <EnterLayout imgsrc={LoginSvg as string}>
-        {/* complete login component and replace it with signup component */}
-        <SignUp/>
+      {/* complete login component and replace it with signup component */}
+      <Login />
     </EnterLayout>
   )
 }
