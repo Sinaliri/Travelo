38e4922c447f3d5d56610e0286d89b60bc2a2db6
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-06-15 23:51:43 +0330

    added create trip page

---
+++ Components/modules/CreateTripComponent/CreateTripComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateTrip from '../../../assets/images/CreateTrip.png'
+import Image from 'next/image';
+
+const CreateTripComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CreateTripComponent;
---
+++ pages/create-trip/index.tsx
@@ -0,0 +1,10 @@
+import CreateTripComponent from "@/Components/modules/CreateTripComponent/CreateTripComponent";
+
+export default function CreateTrip() {
+
+    return (
+        <div className='CreateTrip' style={{ fontFamily: 'Inter' }}>
+            <CreateTripComponent />
+        </div>
+    )
+}
