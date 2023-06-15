592f0018b3845f0f422224683d4d5c9be4229634
Sina <sina.liri@yahoo.com>
2023-06-15 04:11:11 +0330

    Profile layout complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,32 @@
 //theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-    
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 //core
 import "primereact/resources/primereact.min.css";
 
 //icons
-// import "primeicons/primeicons.css";                                         
+// import "primeicons/primeicons.css";
 //primeflex
-import "/node_modules/primeflex/primeflex.css"
+import "/node_modules/primeflex/primeflex.css";
 
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import ProfileLayout from "@/Components/layout/ProfileLayout/ProfileLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  console.log(router.pathname.split("/")[1]);
+  const url: string = router.pathname.split("/")[1];
+
+  if (url === "profile") {
+    return (
+      <ProfileLayout>
+        <Component {...pageProps} />
+      </ProfileLayout>
+    );
+  } else {
+    return <Component {...pageProps} />;
+  }
 }
---
+++ pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import ProfileLayout from '@/Components/layout/ProfileLayout/ProfileLayout'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div></div>
+    )
+}
+
+export default Profile
